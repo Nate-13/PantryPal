729fb8e28a92541b22a7dc471f01e9063f499f0f
Al5knicks <Laurenceaidan5@gmail.com>
2025-04-10 12:50:21 -0400

    update_rest_entry.py
    registered challenges_bp blueprint

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from backend.db_connection import db
-from backend.customers.customer_routes import customers
+from backend.customers.challenges_routes import challenges_bp
 from backend.products.products_routes import products
 from backend.recipes.recipes_routes import recipes
 from backend.ingredients.ingredient_routes import ingredients
@@ -42,7 +42,7 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
+    app.register_blueprint(challenges_bp,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(recipes)
     app.register_blueprint(ingredients)
