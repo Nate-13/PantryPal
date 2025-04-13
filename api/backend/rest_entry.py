178e782e3d1daeee0fa312c4425df757931d2150
Nate-13 <149812512+Nate-13@users.noreply.github.com>
2025-04-13 11:33:12 -0400

    remove unused route registrations  and deleted template files

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 from backend.db_connection import db
 from backend.customers.challenges_routes import challenges_bp
-from backend.products.products_routes import products
 from backend.recipes.recipes_routes import recipes
 from backend.ingredients.ingredient_route import ingredients
-from backend.simple.simple_routes import simple_routes
 from backend.users.users_routes import users
 
 import os
@@ -43,9 +41,7 @@ def create_app():
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
-    app.register_blueprint(simple_routes)
     app.register_blueprint(challenges_bp, url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(recipes)
     app.register_blueprint(ingredients)
     app.register_blueprint(users)
